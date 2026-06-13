2815b28dc563fb6d864454ecc18f2483e83ae4ef
botHacker <71048733+huaihsiungliu@users.noreply.github.com>
2026-06-13 04:25:07 +0000

    01_02b checked in

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(int argc, char** argv) 
+{
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return EXIT_SUCCESS;
+}
